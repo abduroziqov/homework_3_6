6e0ea5e1a28580cd839b92a307873c22e33cac6c
abdurozikov.kuvonchbek@gmail.com <abdurozikov.kuvonchbek@gmail.com>
2023-03-26 00:38:46 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace homework_3_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("x = "); int.TryParse(Console.ReadLine(), out int number);
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine(number + " x " + i + " = " + number * i);
+                }
+            }
+           
+        }
+    }
+}
